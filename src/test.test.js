80ee3e4d3086485ff939857d0eaa6e4803b1fa0e
Chao Zhou <Czhou0k@gmail.com>
2022-11-18 10:08:56 -0500

    added test of assgining to an obj

--- src/test.test.js
+++ src/test.test.js
@@ -3,3 +3,9 @@ const sum = require('./test');
 test('adds num1 + num2 to equal num3', () => {
   expect(sum(2, 3)).toBe(5);
 });
+
+test('assigning to object', () => {
+  const obj = { one: 2 };
+  obj['two'] = 3;
+  expect(obj).toEqual({ one: 2, two: 3 });
+});
