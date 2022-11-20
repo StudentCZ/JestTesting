78539499afa08e2984c3b720ed93ebe43cb61926
Chao Zhou <Czhou0k@gmail.com>
2022-11-20 14:16:50 -0500

    commented out async code

--- src/test.test.js
+++ src/test.test.js
@@ -39,7 +39,16 @@ test('compiling code to go as expected', () => {
   expect(() => compileAndroidCode()).toThrow(Error);
 });
 
-test('the data is peanut butter', async () => {
-  const data = await fetchData();
-  expect(data).toBe('peanut butter');
-});
+// test('the data is peanut butter', async () => {
+//   const data = await fetchData();
+//   expect(data).toBe('peanut butter');
+// });
+
+// test('the fetch fails with an error', async () => {
+//   expect.assertions(1);
+//   try {
+//     await fetchData();
+//   } catch (e) {
+//     expect(e).toMatch('error');
+//   }
+// });
